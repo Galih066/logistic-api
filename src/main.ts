6a270eef6c96c2b67d868b105fa7850eead42874
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-02-04 17:40:52 +0800

    docs(Swagger): setup for swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+	const app = await NestFactory.create(AppModule);
+	const config = new DocumentBuilder()
+		.setTitle('Logistic API')
+		.setDescription('API For Logistic Bussiness and Requirement')
+		.setVersion('1.0')
+		.addTag('logistic')
+		.build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+	SwaggerModule.setup('api', app, documentFactory);
+	await app.listen(3001);
 }
 bootstrap();
